f950ee8f47bd1e5c828aade96e839b9ff3529da7
Vishnu Rajeevan <vrajeevan@roundarch.com>
2014-10-08 15:05:37 -0500

    removing refs to firebase and jq

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-var Firebase = require('firebase');
 var Rx = require('rx');
 var rootUrl = 'https://hacker-news.firebaseio.com';
 var version = '/v0'
-var myRootRef = new Firebase(rootUrl + version);
 
 var topStories = [];
-//myRootRef.child('/topstories').on("value", function(snapshot){
-    //for(var i = 0; i< snapshot.val().length; i++){
-        //myRootRef.child('/item/'+snapshot.val()[i]).on("value", function(story){
-            //topStories[topStories.length] = story.val();
-        //});
-    //}
-//})
-//
-//
-//
 var requestStream = Rx.Observable.returnValue(rootUrl+version);
 requestStream.subscribe(function(requestUrl){
     var responseStream = Rx.Observable.create(function (observer) {
