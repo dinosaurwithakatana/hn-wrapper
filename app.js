2924c1202622bf4dd0be2180b25d2939267d4590
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2014-10-09 22:11:10 -0500

    adding restify and paging support

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+var restify = require('restify');
 var request = require('request');
 var Rx = require('rx');
 var rootUrl = 'https://hacker-news.firebaseio.com';
 var version = '/v0'
 var get = Rx.Observable.fromNodeCallback(request);
 
-var getTopStories = function(maxStories){
+function getTopStories(req, res, next){
+    var topStories = [];
+    var fromStory = req.query.fromStory;
+    var toStory = req.query.toStory;
     get(rootUrl + version + '/topstories.json')
     .map(function(res){
         return res[1];  // get the body
@@ -15,7 +19,8 @@ var getTopStories = function(maxStories){
     .flatMap(function(res) {
         return Rx.Observable.fromArray(res);    //map each of the story item ids into an observable
     })
-    .take(maxStories)       //take the max
+    .skip(fromStory)
+    .take(toStory)       //take the max
     .flatMap(function(res){
         return get(rootUrl + version + '/item/' + res + '.json');   // make the details call on each response
     })
@@ -27,15 +32,27 @@ var getTopStories = function(maxStories){
     })
     .subscribe(
             function (x) {
-                console.log(x.title);
+                topStories[topStories.length] = x;
             },
             function (err) {
                 console.log('Error:  ' + err);
             },
             function () {
-                console.log('Complete');
+                console.dir(topStories[0].title);
+                res.send(topStories);
             }
     );
 }
 
-getTopStories(10);
+var server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/getTopStories', getTopStories); 
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
