29cfb0b01c08c8f555d9fb4e66d73d0c65421d14
Siddharth Sathyam <ssathy2@gmail.com>
2014-11-16 20:29:49 -0600

    Implementing caching using redis, added start script to check if redis server is running, and added a rx redis implementation

---
+++ Utilities/rxredis.js
@@ -0,0 +1,39 @@
+// Redis related
+var redis   = require('redis');
+var redisClient = redis.createClient();
+
+var Rx = require('Rx');
+
+// Returns an observable for a redis value for a given key
+function valueForKey(key){
+	return Rx.Observable.create(function(observer){
+		redisClient.get(key, function(err, reply){
+			if (err)
+				observer.error(err);
+			else {
+				var jsonReply = JSON.parse(reply)
+				observer.onNext(jsonReply);
+				observer.onCompleted();
+			}
+		})	
+	})
+}
+
+// Returns an observable that retuns the value once it's been set in redis
+function setValueForKey(key, value){
+	return Rx.Observable.create(function(observer){
+		var jsonValue = JSON.stringify(value);
+		redisClient.set(key, jsonValue, function(error){
+			if (error)
+				observer.error(error)
+			else {
+				observer.onNext(value);
+				observer.onCompleted();
+			}
+
+		});
+	})
+}
+
+module.exports.valueForKey = valueForKey;
+module.exports.setValueForKey = setValueForKey;
